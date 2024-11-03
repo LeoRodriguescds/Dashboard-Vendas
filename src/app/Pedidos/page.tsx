47fd812a9633416268e85eecfbb80756d669c485
LeoRodriguescds <leonardorodriguescds.2020@gmail.com>
2024-11-03 13:27:21 -0300

    feat: página responsável pelos pedidos

--- src/app/Pedidos/page.tsx
+++ src/app/Pedidos/page.tsx
@@ -1,7 +1,68 @@
+import { ChartPedidos } from "@/components/chart/ChartPedidos";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollText } from "lucide-react";
+
 export default function Pedidos() {
   return (
-    <div>
-      <h1>Pedidos</h1>
+    <div className="sm:ml-14 p-4">
+      <header className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl font-bold">Pedidos</h1>
+      </header>
+      <section className="flex flex-col h-full gap-5">
+        <div className="w-full ">
+          <ChartPedidos />
+        </div>
+        <div className="flex w-full gap-5">
+          <Card className="w-1/3">
+            <CardHeader>
+              <div className="flex items-center justify-center">
+                <CardTitle className="text-lg text-gray-700 sm:text-xl select-none">
+                  Total de Pedidos Mensais
+                </CardTitle>
+                <ScrollText className="ml-auto w-5 h-5" />
+              </div>
+              <CardDescription>Total de pedidos em 30 dias.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="text-base sm:text-lg font-bold">92</p>
+            </CardContent>
+          </Card>
+          <Card className="w-1/3">
+            <CardHeader>
+              <div className="flex items-center justify-center">
+                <CardTitle className="text-lg text-gray-700 sm:text-xl select-none">
+                  Total de Pedidos Mensais
+                </CardTitle>
+                <ScrollText className="ml-auto w-5 h-5" />
+              </div>
+              <CardDescription>Total de pedidos em 30 dias.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="text-base sm:text-lg font-bold">92</p>
+            </CardContent>
+          </Card>
+          <Card className="w-1/3">
+            <CardHeader>
+              <div className="flex items-center justify-center">
+                <CardTitle className="text-lg text-gray-700 sm:text-xl select-none">
+                  Total de Pedidos Mensais
+                </CardTitle>
+                <ScrollText className="ml-auto w-5 h-5" />
+              </div>
+              <CardDescription>Total de pedidos em 30 dias.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="text-base sm:text-lg font-bold">92</p>
+            </CardContent>
+          </Card>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/chart/ChartPedidos.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { ChartLine } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "../ui/chart";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+
+export function ChartPedidos() {
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ];
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "hsl(var(--chart-1))",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "hsl(var(--chart-2))",
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <Card className="w-full md:w-full ">
+      <CardHeader>
+        <div className="flex items-center justify-center">
+          <CardTitle className="text-lg text-gray-700 sm:text-xl select-none">
+            Overview Vendas
+          </CardTitle>
+          <ChartLine className="ml-auto w-5 h-5" />
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <ChartContainer className="h-[400px] w-full" config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <defs>
+              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+            </defs>
+            <Area
+              dataKey="mobile"
+              type="natural"
+              fill="url(#fillMobile)"
+              fillOpacity={0.4}
+              stroke="var(--color-mobile)"
+              stackId="a"
+            />
+            <Area
+              dataKey="desktop"
+              type="natural"
+              fill="url(#fillDesktop)"
+              fillOpacity={0.4}
+              stroke="var(--color-desktop)"
+              stackId="a"
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
